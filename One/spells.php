4c6f09e1e8b28655de2b85a866568f46926efc92
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2020-09-19 22:54:51 -0600

    Create spells.php

---
+++ One/spells.php
@@ -0,0 +1,168 @@
+<table style="font-size: 14pt; margin: auto; text-align: center; font-family: alegreya;">
+  <tr>
+    <th align="center" colspan="4" style="font-size: 18pt;">Spells</th>
+  </tr>
+  <tr>
+    <th align="center" colspan="4" style="font-size: 10pt; font-weight: normal;">N'Komo does not have any spells!</th>
+  </tr>
+</table>
+<table style="font-size: 14pt; margin: auto; text-align: center; font-family: alegreya;">
+  <tr>
+    <th align="center" colspan="4" style="font-size: 18pt;">Spell-Like Abilities
+    </th>
+  </tr>
+  <tr style="font-size: 16pt; font-weight: bold; background: #8e0f0f; color: #efb839;">
+    <td class="a2"><span class="tooltip">Source<span class="text"><span class="orange">∙</span> = RACE</span></td>
+    <td class="q">Name</td>
+    <td class="a">Description</td>
+    <td class="a1">Allotments</td>
+  </tr>
+  <tr>
+    <td class="a2 orange">∙</td>
+    <td class="q">Darkness</td>
+    <td class="a">Evocation [Darkness]
+<br/>Level:	Brd 2, Clr 2, Sor/Wiz 2
+<br/>Components:	V, M/DF
+<br/>Casting Time:	1 standard action
+<br/>Range:	Touch
+<br/>Target:	Object touched
+<br/>Duration:	10 min./level (D)
+<br/>Saving Throw:	None
+<br/>Spell Resistance:	No
+<br/>This spell causes an object to radiate shadowy illumination out to a 20-foot radius. All creatures in the area gain concealment (20% miss chance). Even creatures that can normally see in such conditions (such as with darkvision or low-light vision) have the miss chance in an area shrouded in magical darkness.
+<br/><br/>
+Normal lights (torches, candles, lanterns, and so forth) are incapable of brightening the area, as are light spells of lower level. Higher level light spells are not affected by darkness.
+<br/><br/>
+If darkness is cast on a small object that is then placed inside or under a lightproof covering, the spell’s effect is blocked until the covering is removed.
+<br/><br/>
+Darkness counters or dispels any light spell of equal or lower spell level.
+<br/><br/>
+Arcane Material Component
+<br/>A bit of bat fur and either a drop of pitch or a piece of coal.
+	</td>
+    <td class="a1"><input type="checkbox">1/day</td>
+  </tr>
+  <tr>
+    <td class="a2 orange">∙</td>
+    <td class="q">Charm Monster</td>
+    <td class="a">Enchantment (Charm) [Mind-Affecting]
+<br/>Level:	Brd 3, Sor/Wiz 4
+<br/>Target:	One living creature
+<br/>Duration:	One day/level
+<br/><br/>This spell functions like charm person, except that the effect is not restricted by creature type or size.
+</td>
+    <td class="a1"><input type="checkbox">1/week</td>
+  </tr>
+  <tr>
+    <td class="a2 orange">∙</td>
+    <td class="q">Gaseous Form</td>
+    <td class="a">Transmutation
+<br/>Level:	Air 3, Brd 3, Sor/Wiz 3
+<br/>Components:	S, M/DF
+<br/>Casting Time:	1 standard action
+<br/>Range:	Touch
+<br/>Target:	Willing corporeal creature touched
+<br/>Duration:	2 min./level (D)
+<br/>Saving Throw:	None
+<br/>Spell Resistance:	No
+<br/>The subject and all its gear become insubstantial, misty, and translucent. Its material armor (including natural armor) becomes worthless, though its size, Dexterity, deflection bonuses, and armor bonuses from force effects still apply. The subject gains damage reduction 10/magic and becomes immune to poison and critical hits. It can’t attack or cast spells with verbal, somatic, material, or focus components while in gaseous form. (This does not rule out the use of certain spells that the subject may have prepared using the feats Silent Spell, Still Spell, and Eschew Materials.) The subject also loses supernatural abilities while in gaseous form. If it has a touch spell ready to use, that spell is discharged harmlessly when the gaseous form spell takes effect.
+<br/><br/>
+A gaseous creature can’t run, but it can fly at a speed of 10 feet (maneuverability perfect). It can pass through small holes or narrow openings, even mere cracks, with all it was wearing or holding in its hands, as long as the spell persists. The creature is subject to the effects of wind, and it can’t enter water or other liquid. It also can’t manipulate objects or activate items, even those carried along with its gaseous form. Continuously active items remain active, though in some cases their effects may be moot.
+<br/><br/>
+Arcane Material Component
+<br/>A bit of gauze and a wisp of smoke.
+</td>
+    <td class="a1"><input type="checkbox">1/week</td>
+  </tr>
+  <tr>
+    <td class="a2 orange">∙</td>
+    <td class="q">Invisibility</td>
+    <td class="a">Illusion (Glamer)
+<br/>Level:	Brd 2, Sor/Wiz 2, Trickery 2
+<br/>Components:	V, S, M/DF
+<br/>Casting Time:	1 standard action
+<br/>Range:	Personal or touch
+<br/>Target:	You or a creature or object weighing no more than 100 lb./level
+<br/>Duration:	1 min./level (D)
+<br/>Saving Throw:	Will negates (harmless) or Will negates (harmless, object)
+<br/>Spell Resistance:	Yes (harmless) or Yes (harmless, object)
+<br/>The creature or object touched becomes invisible, vanishing from sight, even from darkvision. If the recipient is a creature carrying gear, that vanishes, too. If you cast the spell on someone else, neither you nor your allies can see the subject, unless you can normally see invisible things or you employ magic to do so.
+<br/><br/>
+Items dropped or put down by an invisible creature become visible; items picked up disappear if tucked into the clothing or pouches worn by the creature. Light, however, never becomes invisible, although a source of light can become so (thus, the effect is that of a light with no visible source). Any part of an item that the subject carries but that extends more than 10 feet from it becomes visible.
+<br/><br/>
+Of course, the subject is not magically silenced, and certain other conditions can render the recipient detectable (such as stepping in a puddle). The spell ends if the subject attacks any creature. For purposes of this spell, an attack includes any spell targeting a foe or whose area or effect includes a foe. (Exactly who is a foe depends on the invisible character’s perceptions.) Actions directed at unattended objects do not break the spell. Causing harm indirectly is not an attack. Thus, an invisible being can open doors, talk, eat, climb stairs, summon monsters and have them attack, cut the ropes holding a rope bridge while enemies are on the bridge, remotely trigger traps, open a portcullis to release attack dogs, and so forth. If the subject attacks directly, however, it immediately becomes visible along with all its gear. Spells such as bless that specifically affect allies but not foes are not attacks for this purpose, even when they include foes in their area.
+<br/><br/>
+Invisibility can be made permanent (on objects only) with a permanency spell.
+<br/><br/>
+Arcane Material Component
+<br/>An eyelash encased in a bit of gum arabic.</td>
+    <td class="a1"><input type="checkbox"><input type="checkbox">2/week</td>
+  </tr>
+  <tr>
+    <td class="a2 orange">∙</td>
+    <td class="q">Fly</td>
+    <td class="a">Transmutation
+<br/>Level:	Sor/Wiz 3, Travel 3
+<br/>Components:	V, S, F/DF
+<br/>Casting Time:	1 standard action
+<br/>Range:	Touch
+<br/>Target:	Creature touched
+<br/>Duration:	1 min./level
+<br/>Saving Throw:	Will negates (harmless)
+<br/>Spell Resistance:	Yes (harmless)
+<br/>The subject can fly at a speed of 60 feet (or 40 feet if it wears medium or heavy armor, or if it carries a medium or heavy load). It can ascend at half speed and descend at double speed, and its maneuverability is good. Using a fly spell requires only as much concentration as walking, so the subject can attack or cast spells normally. The subject of a fly spell can charge but not run, and it cannot carry aloft more weight than its maximum load, plus any armor it wears.
+<br/><br/>
+Should the spell duration expire while the subject is still aloft, the magic fails slowly. The subject floats downward 60 feet per round for 1d6 rounds. If it reaches the ground in that amount of time, it lands safely. If not, it falls the rest of the distance, taking 1d6 points of damage per 10 feet of fall. Since dispelling a spell effectively ends it, the subject also descends in this way if the fly spell is dispelled, but not if it is negated by an antimagic field.
+<br/><br/>
+Arcane Focus
+<br/>A wing feather from any bird.</td>
+    <td class="a1"><input type="checkbox"><input type="checkbox">2/week</td>
+  </tr>
+</table>
+<script src="../jquery-latest.js"></script>
+<script>
+ $(function() {
+$('.majorpoints').click(function() {
+        $(this).find('.hiders').toggle();
+      });
+	  });
+</script>
+<style>
+  #spells .q {
+  background: #00000033;
+  border: 1px #8a8a8a solid;
+  width: 100px;
+  }
+  #spells .a {
+  background: #ffffff33;
+  border: 1px #8a8a8a solid;
+  font-size: 12pt;
+  width: 70%;
+  }
+  #spells .a1 {
+  background: #7375d433;
+  border: 1px #8a8a8a solid;
+  width: 100px;
+  }
+  #spells .a2 {
+  background: #2f2f2f82;
+  border: 1px #8a8a8a solid;
+  width: 50px;
+  }
+  .orange {
+	  color: orange;
+	  font-size: 40pt;
+  }
+  .blue {
+	  color: blue;
+	  font-size: 40pt;
+  }
+  .green {
+	  color: yellowgreen;
+	  font-size: 40pt;
+  }
+  .grey {
+	  color: silver;
+	  font-size: 40pt;
+  }
+</style>
